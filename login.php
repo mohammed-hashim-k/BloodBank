57d5d32a33023bacfaef64cc08129b7cb7c0ec86
callistopan <callistopan@gmail.com>
2021-11-29 08:22:05 +0530

    initial

--- login.php
+++ login.php
@@ -8,17 +8,17 @@
 		$con = mysqli_connect($localhost,$root,$password) or die('Could not connect to database');
 		mysqli_select_db($con,"blood_bank");
         
-        $username = $_POST['userid'];
+        $userid = $_POST['userid'];
         $password = $_POST['password'];
         
         $query = "SELECT * FROM user WHERE userid = '$userid' AND password = '$password'";
         $result = mysqli_query($con,$query);
         $row = mysqli_fetch_array($result);
         
-        if($row['userid'] == $useid && $row['password'] == $password) {
+        if($row['userid'] == $userid && $row['password'] == $password) {
             $_SESSION['userid'] = $userid;
             $_SESSION['password'] = $password;
-            header("location: index.php");
+            header("location: index.html");
         } else {
             echo "<script>alert('Invalid userid or password')</script>";
         }
