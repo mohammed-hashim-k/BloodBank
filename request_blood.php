388e3563484aaa1eb04639af6decca6b95d0d4a8
Akash Wilson M <akashwilsonm@gmail.com>
2021-12-02 10:30:24 +0530

    request rejection if less unit in stock

--- request_blood.php
+++ request_blood.php
@@ -15,22 +15,29 @@
     $admin_id='1';
     $status="pending";
     $action="none";
-    $sql="INSERT INTO blood_request VALUES('$request_id','$requestor_id','$unit','$request_date','$reason','$status','$action','$admin_id')";
 
+    $row = mysqli_fetch_array(mysqli_query($con, "SELECT unit FROM blood_stock WHERE blood_type IN (SELECT blood_type FROM user WHERE userid = '$requestor_id') "));
 
+    if($row['unit'] > $unit) {
+        $sql="INSERT INTO blood_request VALUES('$request_id','$requestor_id','$unit','$request_date','$reason','$status','$action','$admin_id')";
 
-
-
-
-
-    if($con->query($sql)==TRUE){
-        echo "Requested";
+        if($con->query($sql)==TRUE){
+            echo "Requested";
+        }
+        else{
+            echo "Error: " . $sql . "<br>" . $con->error;
+        }
     }
+
     else{
-        echo "Error: " . $sql . "<br>" . $con->error;
+        $sql = "INSERT INTO blood_request VALUE('$request_id','$requestor_id','$unit','$request_date','$reason','rejected','Stock Low','$admin_id')";
+
+        if($con->query($sql) == TRUE){
+            echo "REJECTED : Low blood Stock";
+        }
+        else{
+            echo "Error: " . $sql . "<br>" . $con->error;
+        }
     }
-
-
-
-   }
+}
     ?>
